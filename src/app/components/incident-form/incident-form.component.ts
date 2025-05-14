19b35bced99933e8fdfc9c01b4dc39bcdd25f68e
Paulbuv <paulbuv@gmail.com>
2025-05-14 11:00:01 +0200

    Ingredient-form files added

---
+++ src/app/components/incident-form/incident-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-incident-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './incident-form.component.html',
+  styleUrl: './incident-form.component.css'
+})
+export class IncidentFormComponent {
+
+}
