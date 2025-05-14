a5668da5d378b234294ca5b3ef891214bf623523
Paulbuv <paulbuv@gmail.com>
2025-05-14 11:19:11 +0200

    Add Incident Form added (component, template and quick css)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { IncidentListComponent } from './components/incident-list/incident-list.component'
+import { IncidentFormComponent } from './components/incident-form/incident-form.component'
+
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
     IncidentListComponent,
+    IncidentFormComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
--- src/app/components/incident-form/incident-form.component.ts
+++ src/app/components/incident-form/incident-form.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { IncidentService } from '../../services/incident.service';
+import { Incident } from '../../models/incident';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-incident-form',
-  standalone: true,
-  imports: [],
-  templateUrl: './incident-form.component.html',
-  styleUrl: './incident-form.component.css'
+  selector:       'app-incident-form',
+  templateUrl:    './incident-form.component.html',
+  standalone :    true,
+  imports : [
+    ReactiveFormsModule,
+    CommonModule
+  ],
+  styleUrls:      ['./incident-form.component.css']
 })
 export class IncidentFormComponent {
+  incidentForm: FormGroup;
 
+  constructor(private fb: FormBuilder, private incidentService: IncidentService) {
+    this.incidentForm = this.fb.group({
+      title: ['', Validators.required],
+      description: [''],
+      priority: ['Low', Validators.required]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.incidentForm.valid) {
+      const newIncident: Incident = {
+        ...this.incidentForm.value,
+        id: 0, // Will be defined in the service
+        status: 'Open' as 'Open'
+      };
+      this.incidentService.addIncident(newIncident);
+      this.incidentForm.reset({ priority: 'Low' });
+    }
+  }
 }
